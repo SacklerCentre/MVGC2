86336fc9dd92880ee78cc04e87f503fa9d9de74b
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2020-07-24 09:16:55 +0100

    remove files in 'maintainer'

--- maintainer/taritup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# $1 is version (eq 1.0), $2 is directory (eg /tmp)
-
-# tar czvf $2/mvgc_v$1.tar.gz --exclude=extra --exclude=testing --exclude=maintainer mvgc_v$1/*
-tar czvf $2/mvgc_v$1.tar.gz --exclude=testing --exclude=maintainer mvgc_v$1/*
