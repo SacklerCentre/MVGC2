6ce398c4a3634f53d9073b397269171515eb8b71
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2021-08-23 17:22:11 +0100

    Fixed 'bdoc' MVGC location, also to check paths'

--- maintainer/bdoc.m
+++ maintainer/bdoc.m
@@ -8,7 +8,7 @@ if nargin < 1 || isempty(fstr)
     return
 end
 
-docroot = fullfile(getenv('LOCALREPO'),'matlab','ncomp','mvgc','docs','html');  % first look in MVGC docs
+docroot = fullfile(getenv('MVGC2_PATH'),'docs','html');  % first look in MVGC docs
 if disdoc(docroot,fstr,browser), return; end
 
 docroot = fullfile(matlabroot,'help','matlab','ref'); % then look in 'matlab/ref' section
@@ -28,8 +28,8 @@ function success = disdoc(docroot,fstr,browser)
 assert(status == 0,'''find'' call failed');
 success = ~isempty(dpath);
 if success
-    dpaths = strsplit(dpath);
-    dpath = dpaths{1};
-    %web(['file://' dpaths{1}],'-browser');
-    system([browser ' ' dpath ' &']);
+	dpaths = strsplit(dpath);
+	dpath = dpaths{1};
+	%web(['file://' dpaths{1}],'-browser');
+	system([browser ' ' dpath ' &']);
 end
