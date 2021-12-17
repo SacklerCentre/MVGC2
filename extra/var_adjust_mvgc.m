c1e7649778523e639abed03a6176868698c5d2a0
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2021-12-17 17:28:44 +0000

    Amended var_adjust_mvgc.m

--- extra/var_adjust_mvgc.m
+++ extra/var_adjust_mvgc.m
@@ -4,12 +4,12 @@ function [AA,FF,iters] = var_adjust_mvgc(A,V,x,y,F,maxi);
 % such that GC(y -> x) is equal to specified value F, based on
 % a binary chop. Terminate when binary chop cannot proceed any
 % further, or maximum iterations maxi is exceeded. Result should
-% be tested; that is, check that iters <= maxi and that abs(F-FF)
-% is tolerably small (or zero).
+% be tested; that is, check that FF lies as close as required to
+% target FT.
 
 assert(F >= 0,'Target GC must be non-negative!');
 
-if nargin < 6 || isempty(maxi), maxi = 500; end
+if nargin < 6 || isempty(maxi), maxi = 1000;  end
 
 % Initialise
 
@@ -35,12 +35,12 @@ f = 0;
 FF = 0;
 while FF < F
 	iters = iters+1;
+	if iters > maxi, return; end % timed out
 	f = f+1;
 	AA = A;
 	AA(x,y,:) = f*Axy;
 	AA = specnorm(AA,rho);
 	FF = var_to_mvgc(AA,V,x,y);
-	if iters > maxi, return; end
 end
 fmin = f-1;
 fmax = f;
@@ -49,15 +49,16 @@ fmax = f;
 % (i.e., to machine precision w.r.t. f) or as far as necessary (i.e., FF is
 % within machine precision of F).
 
+htol = eps/2;
 while true
 	fpre = f;                         % previous value of f
 	f = (fmin+fmax)/2;                % binary chop
 	if abs(fpre-f) <= eps, break; end % no point going any further!
 	iters = iters+1;
+	if iters > maxi, return; end % timed out
 	AA = A;
 	AA(x,y,:) = f*Axy;
 	AA = specnorm(AA,rho);
 	FF = var_to_mvgc(AA,V,x,y);
-	if FF > F+eps, fmax = f; elseif FF < F-eps, fmin = f; else, return; end % new box
-	if iters > maxi, return; end
+	if FF > F+htol, fmax = f; elseif FF < F-htol, fmin = f; else, break; end % new box
 end
