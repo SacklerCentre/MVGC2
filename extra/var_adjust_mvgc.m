3d32d05a42941e3ead275f20ede5e079cc8a0352
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2021-12-20 10:08:34 +0000

    Yet another tweak to var_adjust_mvgc.m

--- extra/var_adjust_mvgc.m
+++ extra/var_adjust_mvgc.m
@@ -1,32 +1,28 @@
 function [AA,Fxy,iters] = var_adjust_mvgc(A,V,x,y,F,maxi,ftol);
 
-% Find VAR coefficients AA based on supplied VAR parameters
-% (A,V), such that Fxy, the conditional Granger causality from
-% y to x, is within a given tolerance of the specified value F.
+% Find VAR coefficients AA based on supplied VAR parameters (A,V),
+% such that Fxy, the conditional Granger causality from% y to x,
+% is within a given tolerance ftol of the specified value F. The
+% spectral radius of the model is preserved.
 %
-% The algorithm implements a binary chop. If no tolerance is
-% provided, the search continues until the binary chop can
-% proceed no further, or the maximum number of iterations is
-% exceeded. If a tolerance is provided, the search continues
-% until the tolerance is met, the binary chop cannot proceed,
-% or the maximum number of iterations is exceeded. In either
-% case, the returned result Fxy and number of iterations iters
-% should be checked.
+% The algorithm implements a binary chop. If the tolerance ftol
+% is zero, the search continues until the binary chop can
+% proceed no further, or the maximum number of iterations maxi
+% is exceeded. Otherwise, the search continues until the tolerance
+% is met, the binary chop cannot proceed, or the maximum number
+% of iterations is exceeded. In either case, the returned result
+% Fxy and number of iterations iters should be checked.
 
 assert(F >= 0,'Target GC must be non-negative!');
 
+% Reasonable defaults
+
 if nargin < 6 || isempty(maxi), maxi = 1000;  end
+if nargin < 7 || isempty(ftol), ftol = 1e-10; end
 
-if nargin < 7 || isempty(ftol);
-	hftol = 0;
-else
-	hftol = ftol/2;
-end
-
-% Initialise
+% Spectral radius
 
 rho = specnorm(A);
-iters = 0;
 
 % F == 0 is a special case:
 
@@ -35,6 +31,7 @@ if F == 0
 	AA(x,y,:) = 0;
 	AA = specnorm(AA,rho);
 	Fxy = 0;
+	iters = 1;
 	return
 end
 
@@ -42,9 +39,9 @@ end
 % A(x,y,:); we try to find a multiplicative factor f which delivers the
 % required GC. Firstly we find a ceiling for f.
 
-f   = 0;
 Axy = A(x,y,:);
-Fxy = 0;
+f = 0;
+iters = 0;
 while iters <= maxi
 	f = f+1;
 	AA = A;
@@ -54,13 +51,14 @@ while iters <= maxi
 	if Fxy > F, break; end % have a ceiling for f
 	iters = iters+1;
 end
-
+f
 % We've boxed the multiplicative factor between f-1 and f; now binary chop
 % as far as possible (i.e., to machine precision w.r.t. the factor f) or
 % until Fxy is within the specified tolerance of F.
 
 fmin = f-1;
 fmax = f;
+hftol = ftol/2;
 while iters <= maxi
 	fpre = f;                         % previous value of f
 	f = (fmin+fmax)/2;                % binary chop
@@ -72,3 +70,5 @@ while iters <= maxi
 	if Fxy > F+hftol, fmax = f; elseif Fxy < F-hftol, fmin = f; else, break; end % terminate if tolerance met
 	iters = iters+1;
 end
+
+f
