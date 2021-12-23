5bb82734a515f1651b88066a6c4fac40b07fb378
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2021-12-23 11:53:40 +0000

    Updated documentation of cov2corr and cov2pcorr

--- utils/cov2corr.m
+++ utils/cov2corr.m
@@ -1,25 +1,30 @@
 function [R,p,pval] = cov2corr(V,N)
 
 % Calculate pairwise (Pearson) correlation matrix from covariance
-% matrix. P-values are calculated for a 2-tailed test.
+% matrix. If the covariance matrix is an unbiased estimate from
+% multivariate data, p-values are calculated for a 2-tailed test
+% (i.e. for the null hypothesis of zero correlation); n(n-1)/2
+% non-independent hypotheses must be assumed (see routine
+% 'significance').
 %
 % The algorithm attempts to ensure a positive-definite result; if
 % the covariance matrix is not positive-definite, we fall back on
-% a "safe" method.
+% a "safe" method, for which the result may be inaccurate and not
+% positive-definite; the flag p should thus always be checked.
 %
-% V - sample covariance matrix (unbiased estimator)
+% V - covariance matrix
 % N - sample size (number of data observations)
 
-[n,n1] = size(V);
-assert(ismatrix(V) && n1 == n,'Covariance matrix must be square');
+n = size(V,1);
+assert(issymmetric(V),'Covariance matrix must be square symmetric');
 
 R  = NaN(n);
 pval = NaN(n);
 
 [R,p] = chol(V); % right Cholesky factor
-if p > 0 % fall back on "safe" method (doesn't ensure positive-definite result)
+if p > 0 % fall back on "safe" method (possibly inaccurate, and doesn't ensure positive-definite result)
 	d = 1./sqrt(diag(V));
-	R = bsxfun(@times,bsxfun(@times,d,V),d');
+	R = symmetrise(bsxfun(@times,bsxfun(@times,d,V),d'));
 else      % ensures positive-definite result
 	R = bsxfun(@rdivide,R,sqrt(sum(R.*R)));
 	R = R'*R;
--- utils/cov2pcorr.m
+++ utils/cov2pcorr.m
@@ -1,33 +1,41 @@
 function [R,p,pval] = cov2pcorr(V,N)
 
 % Calculate pairwise partial (Pearson) correlation matrix from
-% covariance matrix using inversion method. P-values are calculated
-% for a 2-tailed test.
+% covariance matrix using inversion method. If the covariance
+% matrix is an unbiased estimate from multivariate data, p-values
+% are calculated for a 2-tailed test (i.e. for the null hypothesis
+% of zero partial correlation); n(n-1)/2 non-independent hypotheses
+% must be assumed (see routine 'significance').
 %
 % The algorithm attempts to ensure a positive-definite result; if
 % the covariance matrix is not positive-definite, we fall back on
-% a "safe" method. If the covariance matrix is calculated from time-
-% series data, it is recommended that the data is normalised by
-% variance, as this improves stability.
+% a "safe" method, for which the result may be inaccurate and not
+% positive-definite; the flag p should thus always be checked.
 %
-% V - sample covariance matrix (unbiased estimator)
+% V - covariance matrix
 % N - sample size (number of data observations)
 %
-% NOTE: The pairwise-conditional mutual information under Gaussian
+% NOTE 1: For partial correlation calculated from multivariate data,
+% this routine may be less accurate than Matlab's 'partialcorr'
+% function - but it is definitely more efficient!
+%
+% NOTE 2: The pairwise-conditional mutual information under Gaussian
 % assumptions is -log(1-R.*conj(R)), with an asymptotic chi^2(1)
 % distribution.
 
-[n,n1] = size(V);
-assert(ismatrix(V) && n1 == n,'Covariance matrix must be square');
+n = size(V,1);
+assert(issymmetric(V),'Covariance matrix must be square symmetric');
 
 R  = NaN(n);
 pval = NaN(n);
 
 [R p] = chol(V); % right Cholesky factor
-if p > 0 % fall back on "safe" method (doesn't ensure positive-definite result)
+if p > 0 % fall back on "safe" method (possibly inaccurate, and doesn't ensure positive-definite result)
+	warning('off','MATLAB:nearlySingularMatrix'); % if not positive-definite, expect problems anyway!
 	R = inv(V);
+	warning('on','MATLAB:nearlySingularMatrix');
 	d = 1./sqrt(diag(R));
-	R = -bsxfun(@times,bsxfun(@times,d,R),d');
+	R = symmetrise(-bsxfun(@times,bsxfun(@times,d,R),d'));
 else
 	R = inv(R);
 	R = bsxfun(@times,1./sqrt(sum(R.*conj(R),2)),R);
