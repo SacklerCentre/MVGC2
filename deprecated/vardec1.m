6db8b9f3467e807258129a3bf7155aae5c660366
lcbarnett <l.c.barnett@sussex.ac.uk>
2023-04-21 13:10:32 +0100

    Amended vardec.m routine

---
+++ deprecated/vardec1.m
@@ -0,0 +1,59 @@
+function [A,a] = vardec1(A,lam,plotm)
+
+% VAR coefficients sequence decay
+%
+% [lam,a] = vardec(A) returns the fitted exponential decay factor
+%           for the given VAR coefficients sequence A.
+%
+% [A,a]   = vardec(A,lam) returns the coefficient sequence A weighted
+%           so that the decay factor = lam.
+%
+% Optionally, the norms at each lag are returned in a.
+%
+% Note that the second calling form will NOT in general preserve the
+% spectral radius of the coefficients sequence.
+
+p = size(A,3);
+a = zeros(p,1);
+for k = 1:p
+	a(k) = norm(A(:,:,k));
+end
+
+noweight = nargin < 2 || isempty(lam);
+
+if noweight % no decay factor supplied; return fitted decay factor
+
+	parms = [(1:p)' ones(p,1)]\log(a); % fit to exponential (linear regression = OLS)
+	A = -parms(1);                     % this is the fitted decay factor lam
+
+else        % weight the AR coefficients so decay = lam
+
+	aa = exp(-lam*(1:p)');
+	for k = 1:p
+		A(:,:,k) = (aa(k)/a(k))*A(:,:,k);
+	end
+
+end
+
+if nargin > 2 && ~isempty(plotm) % we're going to plot
+	if ischar(plotm) % plotm is gpterm
+		if noweight
+			gp_qplot((1:p)',a,[],'set xlabel "k"\nset ylabel "|A|"\nset xtics 1\nunset key\nset grid\nset title "VAR coefficients: magnitude vs lag"',plotm);
+		else
+			gp_qplot((1:p)',[a aa],{'old','new'},'set xlabel "k"\nset ylabel "|A|"\nset xtics 1\nset key top right\nset grid\nset title "VAR coefficients: magnitude vs lag"',plotm);
+		end
+	elseif plotm
+		if plotm == 0, figure; else, figure(plotm); end; clf;
+		if noweight
+			plot((1:p)',a);
+		else
+			plot((1:p)',[a aa]);
+			legend('old','new');
+		end
+		xlabel('$k$','Interpreter','latex');
+		y=ylabel('$\Vert A_k \Vert$','rot',0,'Interpreter','latex');
+		set(y, 'position', get(y,'position')-[0.2,0,0]);
+		title('VAR coefficients: magnitude vs lag');
+		grid on
+	end
+end
--- utils/vardec.m
+++ utils/vardec.m
@@ -1,17 +1,13 @@
-function [A,a] = vardec(A,lam,plotm)
+function lam = vardec(A,plotm,pres)
 
-% VAR coefficients sequence decay
+% VAR coefficients sequence decay: returns the fitted exponential
+% decay factor for the given VAR coefficients sequence.
 %
-% [lam,a] = vardec(A) returns the fitted exponential decay factor
-%           for the given VAR coefficients sequence A.
+% A       - VAR coefficients sequence (3D array, last index is lag)
+% plotm   - empty: don't plot, integer: Matlab plot to figure n (if zero, use next); string: Gnuplot terminal (may be empty)
+% pres    - resolution for plotting exponential fit (default: 1000)
 %
-% [A,a]   = vardec(A,lam) returns the coefficient sequence A weighted
-%           so that the decay factor = lam.
-%
-% Optionally, the norms at each lag are returned in a.
-%
-% Note that the second calling form will NOT in general preserve the
-% spectral radius of the coefficients sequence.
+% lam     - VAR coefficients exponential decay factor (empty for "natural" decay)
 
 p = size(A,3);
 a = zeros(p,1);
@@ -19,41 +15,44 @@ for k = 1:p
 	a(k) = norm(A(:,:,k));
 end
 
-noweight = nargin < 2 || isempty(lam);
+parms = [(1:p)' ones(p,1)]\log(a); % fit to exponential - linear regression (OLS)
+lam = -parms(1);                   % fitted exponential decay factor
 
-if noweight % no decay factor supplied; return fitted decay factor
-
-	parms = [(1:p)' ones(p,1)]\log(a); % fit to exponential (linear regression = OLS)
-	A = -parms(1);                     % this is the fitted decay factor lam
-
-else        % weight the AR coefficients so decay = lam
-
-	aa = exp(-lam*(1:p)');
-	for k = 1:p
-		A(:,:,k) = (aa(k)/a(k))*A(:,:,k);
-	end
-
-end
-
-if nargin > 2 && ~isempty(plotm) % we're going to plot
+if nargin > 1 && ~isempty(plotm) % we're going to plot
+	if nargin < 3 || isempty(pres), pres = 1000; end
+	mu =  parms(2); % fitted exponential offset
+	pp = (1:p)';
+	pf = linspace(0,p+1,pres)';
+	af = exp(mu-lam*pf);
 	if ischar(plotm) % plotm is gpterm
-		if noweight
-			gp_qplot((1:p)',a,[],'set xlabel "k"\nset ylabel "|A|"\nset xtics 1\nunset key\nset grid\nset title "VAR coefficients: magnitude vs lag"',plotm);
-		else
-			gp_qplot((1:p)',[a aa],{'old','new'},'set xlabel "k"\nset ylabel "|A|"\nset xtics 1\nset key top right\nset grid\nset title "VAR coefficients: magnitude vs lag"',plotm);
-		end
+		gpname = 'vardec';
+		gpstem = fullfile(tempdir,gpname);
+		gpdat = {[pp a],[pf af]};
+		gp_write(gpstem,gpdat);
+		gp = gp_open(gpstem,plotm);
+		fprintf(gp,'datfile = "%s.dat"\n',gpname);
+		fprintf(gp,'set title "\\\\large VAR coefficients magnitude vs lag"\n');
+		fprintf(gp,'set key box top right Left rev spacing 1.3 height 0.5\n');
+		fprintf(gp,'set xr [0:%d]\n',p+1);
+		fprintf(gp,'set xlabel "lags ($k$)"\n');
+		fprintf(gp,'set ylabel "VAR coefficients magnitude" rot\n');
+		fprintf(gp,'set key top right Left rev\n');
+		fprintf(gp,'set grid\n');
+		fprintf(gp,'plot datfile i 0 u 1:2 w points pt 7 ps 0.8 t "$\\\\Vert A_k \\\\Vert$", ');
+		fprintf(gp,'datfile i 1 u 1:2 w lines lw 3 t "exponential fit ($\\\\lambda = %g$)\n',lam);
+		gp_close(gp,gpstem,plotm);
 	elseif plotm
 		if plotm == 0, figure; else, figure(plotm); end; clf;
-		if noweight
-			plot((1:p)',a);
-		else
-			plot((1:p)',[a aa]);
-			legend('old','new');
-		end
-		xlabel('$k$','Interpreter','latex');
-		y=ylabel('$\Vert A_k \Vert$','rot',0,'Interpreter','latex');
+		plot(pp,a,'.','markersize',12);
+		hold on
+		plot(pf,af);
+		hold off
+		xlabel('lags ($k$)','Interpreter','latex');
+		y=ylabel('VAR coefficients magnitude','Interpreter','latex');
 		set(y, 'position', get(y,'position')-[0.2,0,0]);
-		title('VAR coefficients: magnitude vs lag');
+		legend('$\Vert A_k\Vert$',sprintf('exponential fit (%c\\lambda = %g%c)','$',lam,'$'),'Interpreter','latex');
+		xlim([0,p+1]);
+		title(sprintf('VAR coefficients magnitude vs lag\n'),'fontsize',11,'fontweight','normal');
 		grid on
 	end
 end
